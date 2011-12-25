550bbd66b4678085a2187b407ba8dcca762b7d55
qq280833822@gmail.com <qq280833822@gmail.com@26ae86c0-a76d-e100-0196-5da9ae7b0f26>
2011-12-25 14:05:30 +0000

    min2phase instead

--- WeisuoCubeAssistant/src/scramble/_333Scramble.java
+++ WeisuoCubeAssistant/src/scramble/_333Scramble.java
@@ -1,8 +1,15 @@
 package scramble;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
 import javax.microedition.lcdui.Form;
 
-import util.TwoPhaseSearch;
+//import util.TwoPhaseSearch;
+import cs.min2phase.Search;
+import cs.min2phase.Tools;
 
 public class _333Scramble extends Scramble {
 
@@ -23,8 +30,36 @@ public class _333Scramble extends Scramble {
 	}
 
 	public String scramble() {
+		if (!Tools.inited) {
+			System.out.println("Initialization");
+			try {
+				FileConnection file;
+				file = (FileConnection) Connector.open(
+						"file:///e:/WCA/twophase.data", Connector.READ);
+				DataInputStream dis;
+				if (file.exists() && Tools.initFrom(dis = file.openDataInputStream())) {
+					dis.close();
+				} else {
+					file = (FileConnection) Connector.open("file:///e:/WCA/");
+					if (!file.exists()) {
+						file.mkdir();
+					}
+					file = (FileConnection) Connector
+							.open("file:///e:/WCA/twophase.data");
+					if (file.exists())
+						file.delete();
+					file.create();
+					DataOutputStream dos;
+					Tools.initTo(dos = file.openDataOutputStream());
+					dos.flush();
+					dos.close();
+				}
+			} catch (Exception e){
+				e.printStackTrace();
+			}		
+		}
 		// TODO Auto-generated method stub
-		scrambleSequence = TwoPhaseSearch.getInstance(prepare).scramble(length);
+		scrambleSequence = new Search().solution(Tools.randomCube(), length, 10000, false, true);
 		return scrambleSequence;
 	}
 
