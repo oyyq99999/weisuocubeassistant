3a9d1ca2fea03da9f2e495ae1395ddd4caa5fc47
superacidxy <superacidxy@26ae86c0-a76d-e100-0196-5da9ae7b0f26>
2011-12-30 06:09:38 +0000

    improve generatorScramble method

--- WeisuoCubeAssistant/src/scramble/Scramble.java
+++ WeisuoCubeAssistant/src/scramble/Scramble.java
@@ -3,12 +3,27 @@ package scramble;
 import java.util.Random;
 
 public abstract class Scramble {
+
 	protected int length = 0;
 	protected String scrambleSequence = null;
 
 	public abstract String scramble();
-	
+
 	protected String generatorScramble(String[][][] generator) {
+		int[][] index = new int[100][3];
+		int totalMoves = 0;
+		for (int i = 0; i < generator.length; i++) {
+			for (int j = 0; j < generator[i][0].length; j++) {
+				for (int k = 0; k < generator[i][1].length; k++) {
+					int t = totalMoves + j * generator[i][1].length + k;
+					index[t][0] = i;
+					index[t][1] = j;
+					index[t][2] = k;
+				}
+			}
+			totalMoves += generator[i][0].length * generator[i][1].length;
+		}
+
 		StringBuffer sequence = new StringBuffer();
 		Random rand = new Random();
 		int lastAxis = -1;
@@ -16,9 +31,10 @@ public abstract class Scramble {
 		for (int i = 0; i < length; i++) {
 			int axis, shift, count;
 			do {
-				axis = rand.nextInt(generator.length);
-				shift = rand.nextInt(generator[axis][0].length);
-				count = rand.nextInt(generator[axis][1].length);
+				int code = rand.nextInt(totalMoves);
+				axis = index[code][0];
+				shift = index[code][1];
+				count = index[code][2];
 			} while (axis == lastAxis && shiftMoved[shift]);
 			sequence.append(generator[axis][0][shift]);
 			sequence.append(generator[axis][1][count]);
