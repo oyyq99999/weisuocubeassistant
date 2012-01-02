927966e93fa1377e77ae7a1c3d009d563db553bb
oyyq99999 <oyyq9999999@163.com>
2012-01-02 19:06:56 +0000

    add exception package

---
+++ WeisuoCubeAssistant/src/system/exception/TimerStateException.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package system.exception;
+
+/**
+ * @author ouyangyunqi
+ *
+ */
+public class TimerStateException extends Exception {
+
+	public TimerStateException() {
+		super();
+	}
+
+	public TimerStateException(String message) {
+		super(message);
+	}
+	
+}
