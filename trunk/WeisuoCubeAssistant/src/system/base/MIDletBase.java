f7c5fdee662c880b46cc1951870972b72b9ba5ee
oyyq99999@gmail.com <oyyq99999@gmail.com@26ae86c0-a76d-e100-0196-5da9ae7b0f26>
2012-01-02 17:51:11 +0000

    add some base classes

--- trunk/WeisuoCubeAssistant/src/system/base/MIDletBase.java
+++ trunk/WeisuoCubeAssistant/src/system/base/MIDletBase.java
@@ -3,31 +3,58 @@ package system.base;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
+/**
+ * @author ouyangyunqi
+ *
+ */
 public abstract class MIDletBase extends MIDlet {
 	
+	/* (non-Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#destroyApp(boolean)
+	 */
 	protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
 		stopApp();
 	}
 	
+	/**
+	 * template method overwrites destroyApp()
+	 * do cleanUp first, then exit
+	 */
 	private void stopApp() {
 		cleanUp();
 		notifyDestroyed();
 	}
 	
+	/**
+	 * do something before exit
+	 */
 	protected abstract void cleanUp();
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#pauseApp()
+	 */
 	protected void pauseApp() {
 	}
 	
+	/* (non-Javadoc)
+	 * @see javax.microedition.midlet.MIDlet#startApp()
+	 */
 	protected void startApp() throws MIDletStateChangeException {
 		runApp();
 	}
 
+	/**
+	 * template method overwrites startApp()
+	 * do something prepared for the app, then run.
+	 */
 	private void runApp() {
 		prepare();
 		go();
 	}
 
+	/**
+	 * done before app starts, default starts a gc thread
+	 */
 	protected void prepare() {
 		new Thread() {
 			public void run() {
@@ -42,5 +69,8 @@ public abstract class MIDletBase extends MIDlet {
 		}.start();
 	}
 
+	/**
+	 *  do something to start the app!
+	 */
 	protected abstract void go();
 }
