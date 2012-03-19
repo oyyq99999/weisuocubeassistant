2ce19ba08e76e90d935f14515d24e45c3dc65a9e
superacidxy <superacidxy@26ae86c0-a76d-e100-0196-5da9ae7b0f26>
2012-03-19 09:29:50 +0000

    enable more details for statistics

--- branches/WeisuoCubeAssistant-old/src/screens/StatsForm.java
+++ branches/WeisuoCubeAssistant-old/src/screens/StatsForm.java
@@ -15,7 +15,7 @@ public class StatsForm extends Form implements CommandListener {
 	private Command detailCommand = new Command("详细信息", Command.OK, 1);
 	private Command removeCommand = new Command("删除最近一次成绩", Command.SCREEN, 1);
 	private Command resetCommand = new Command("删除所有成绩", Command.SCREEN, 2);
-	private StringItem statStringItem = new StringItem("", "");
+	StringItem statStringItem = new StringItem("", "");
 	private Font statFont = Font.getFont(Font.FACE_MONOSPACE, Font.STYLE_PLAIN,
 			Font.SIZE_SMALL);
 	private Displayable backTo = null;
@@ -32,7 +32,7 @@ public class StatsForm extends Form implements CommandListener {
 			this.addCommand(continueCommand);
 		}
 		this.setCommandListener(this);
-		this.statStringItem.setText(GlobalData.stats.toString());
+		this.statStringItem.setText(GlobalData.stats.toString(true));
 		this.statStringItem.setFont(statFont);
 		this.append(statStringItem);
 	}
@@ -40,13 +40,17 @@ public class StatsForm extends Form implements CommandListener {
 	public void commandAction(Command c, Displayable d) {
 		if (c == this.continueCommand || c == this.backCommand) {
 			GlobalData.display.setCurrent(backTo);
+		} else if (c == this.detailCommand) {
+			GlobalData.display.setCurrent(GlobalData.detailStatsForm);
 		} else if (c == this.removeCommand) {
 			GlobalData.stats.pop_back();
-			this.statStringItem.setText(GlobalData.stats.toString());
+			this.statStringItem.setText(GlobalData.stats.toString(true));
+			GlobalData.detailStatsForm.statStringItem.setText(GlobalData.stats
+					.toString(false));
 		} else if (c == this.resetCommand) {
 			GlobalData.stats.reset();
 			this.statStringItem.setText("没有成绩");
+			GlobalData.detailStatsForm.statStringItem.setText("没有成绩");
 		}
 	}
-
 }
